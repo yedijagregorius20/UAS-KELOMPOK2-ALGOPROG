43c8d539e649ae24a4a65eeb62dd177d44d5721f
willyrusli <willy.422023024@civitas.ukrida.ac.id>
2024-01-10 18:30:50 +0700

    uas_willy_rusli_422023024

---
+++ SortingAlgorithms/selection_sort.py
@@ -0,0 +1,8 @@
+numbers = [4, 9, 3, 6, 2, 1]
+for i in range(len(numbers)):
+    min_idx = i
+    for j in range(i+1, len(numbers)):
+        if numbers[j] < numbers[min_idx]:
+            min_idx = j
+    numbers[i], numbers[min_idx] = numbers[min_idx], numbers[i]
+print(numbers)
